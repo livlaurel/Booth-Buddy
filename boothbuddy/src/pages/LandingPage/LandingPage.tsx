dfba2a6ae1137fdba31c63f3eb7a0882a398cf0b
Sebika-K <khulalsebika11@gmail.com>
2025-09-19 17:23:34 -0500

    Add Landing Page UI

--- boothbuddy/src/pages/LandingPage/LandingPage.tsx
+++ boothbuddy/src/pages/LandingPage/LandingPage.tsx
@@ -1,9 +1,32 @@
-function LandingPage() {
+import { Link } from "react-router-dom";
+import logo from "../../assets/logo.png"; 
+
+export default function LandingPage() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold">Landing Page</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white">
+      {/* Logo */}
+      <img src={logo} alt="BoothBuddy Logo" className="w-40 h-40 mb-8" />
+
+      {/* Links */}
+      <div className="space-y-4 text-center">
+        <p>
+          Already have an account?{" "}
+          <Link to="/login" className="text-blue-400 hover:underline">
+            Login
+          </Link>
+        </p>
+        <p>
+          Don’t have an account?{" "}
+          <Link to="/signup" className="text-blue-400 hover:underline">
+            Signup
+          </Link>
+        </p>
+        <p>
+          <Link to="/" className="text-blue-400 hover:underline">
+            Guest Login
+          </Link>
+        </p>
+      </div>
     </div>
   );
 }
-
-export default LandingPage;
