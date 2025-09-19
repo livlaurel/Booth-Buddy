dfe7cb62854b0cd7ed916106cc572688ba6c24bd
Sebika-K <khulalsebika11@gmail.com>
2025-09-19 13:23:15 -0500

    Set up page structure and routing (Landing, Login, Signup)

---
+++ boothbuddy/src/pages/LandingPage/LandingPage.tsx
@@ -0,0 +1,9 @@
+function LandingPage() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold">Landing Page</h1>
+    </div>
+  );
+}
+
+export default LandingPage;
---
+++ boothbuddy/src/pages/SignupPage/SignupPage.tsx
@@ -0,0 +1,9 @@
+function SignupPage() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold">Signup Page</h1>
+    </div>
+  );
+}
+
+export default SignupPage;
