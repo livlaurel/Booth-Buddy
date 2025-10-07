8dba471b7b507a5689f8f1ed3676a64c7b88c85e
Olivia Laurel <OLaurel113@gmail.com>
2025-10-07 18:33:50 -0500

    added header/footer

---
+++ boothbuddy/src/components/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="bg-[#fff9f0] w-full fixed bottom-0 left-0">
+            <div className="flex justify-left mb-5 ml-5"> 
+                <p>
+                    created by: Booth Buddy Studio
+                </p>
+            </div>    
+        </footer>
+    )
+}
+
+export default Footer;
