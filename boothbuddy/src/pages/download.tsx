5894d38299e7b5c885c90e1869ce9edc1608f7be
Olivia Laurel <OLaurel113@gmail.com>
2025-10-07 21:19:14 -0500

    changes to file name

---
+++ boothbuddy/src/pages/download.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/header"
+import Footer from "../components/footer"
+
+function Download() {
+  return (
+    <>
+      <div>
+        <Header />
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default Download
---
+++ boothbuddy/src/pages/photobooth.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/header"
+import Footer from "../components/footer"
+
+function Booth() {
+  return (
+    <>
+      <div>
+        <Header />
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default Booth
