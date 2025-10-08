ffb19ca4cf709c60376545174bd13de50dea7da8
Olivia Laurel <OLaurel113@gmail.com>
2025-10-07 23:34:54 -0500

    styling changes to header/footer

--- boothbuddy/src/components/footer.tsx
+++ boothbuddy/src/components/footer.tsx
@@ -1,9 +1,9 @@
 const Footer = () => {
     return (
-        <footer className="bg-[#fff9f0] w-full fixed bottom-0 left-0">
-            <div className="flex justify-left mb-5 ml-5"> 
+        <footer className="bg-[#fff9f9] w-full fixed bottom-0 left-0">
+            <div className="flex justify-center font-mono text-sm text-zinc-500 mb-5 ml-5"> 
                 <p>
-                    created by: Booth Buddy Studio
+                    Created by: Booth Buddy Studio
                 </p>
             </div>    
         </footer>
--- boothbuddy/src/components/header.tsx
+++ boothbuddy/src/components/header.tsx
@@ -2,16 +2,11 @@ import logo from "../imgs/boothbuddy_logo.png";
 
 const Header = () => {
     return (
-        <main className="bg-[#fff9f0] w-full fixed top-0 left-0">
+        <main className="bg-[#fff9f9] w-full fixed top-0 left-0">
             <nav className="flex justify-between items-center ml-5">
                 <a href="/" className="text-black flex justify-left">
-                    <img src={logo} alt="livia" className="w-35 h-30" />
+                    <img src={logo} alt="logo" className="w-40 h-30" />
                 </a>
-                <div className="flex-grow font-[Arial, Helvetica, sans-serif] flex font-bold underline justify-end items-center">
-                    <div className="flex space-x-8 text-xl">
-                    </div>
-                </div>
-                <div className="w-25 h-25"></div> 
             </nav>    
         </main>
     )
