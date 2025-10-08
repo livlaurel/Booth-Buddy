4ad087ecbcda89689c7f8a3239ddd4b008d6d28c
Olivia Laurel <OLaurel113@gmail.com>
2025-10-08 18:28:56 -0500

    home page img added

--- boothbuddy/src/pages/home.tsx
+++ boothbuddy/src/pages/home.tsx
@@ -1,5 +1,6 @@
 import Header from "../components/header"
 import Footer from "../components/footer"
+import Booth from "../imgs/homebooth.png"
 
 function Home() {
   return (
@@ -7,11 +8,12 @@ function Home() {
       <div className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow flex justify-center items-center">
+          <img src={Booth} alt="logo" className="w-160 h-140" />
           <div className="flex flex-col items-center space-y-4">
             <h1 className="text-3xl font-black text-black">
               Booth Buddy
             </h1>
-            <div>
+            <div className="flex flex-col items-center space-y-2">
               <a href="/#/login" className="text-white bg-black px-4 py-2 rounded hover:bg-gray-800">
                 Login
               </a>
