6bbfcee0e3f19fa1c4708c614f0fb4b405164d5d
Olivia Laurel <OLaurel113@gmail.com>
2025-10-07 21:19:14 -0500

    changes to file name

--- boothbuddy/src/pages/home.tsx
+++ boothbuddy/src/pages/home.tsx
@@ -4,8 +4,13 @@ import Footer from "../components/footer"
 function Home() {
   return (
     <>
-      <div>
+      <div className="flex flex-col min-h-screen">
         <Header />
+        <main className="flex-grow flex justify-center items-center">
+          <p className="text-3xl font-black text-black">
+            Booth Buddy
+          </p>
+        </main>
         <Footer />
       </div>
     </>
---
+++ boothbuddy/src/pages/login.tsx
@@ -0,0 +1,15 @@
+import Header from "../components/header"
+import Footer from "../components/footer"
+
+function Login() {
+  return (
+    <>
+      <div>
+        <Header />
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default Login
