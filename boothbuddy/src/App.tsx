7ec4cbe98756920b9dd743cbed44e6007db08af5
Olivia Laurel <OLaurel113@gmail.com>
2025-10-07 21:19:25 -0500

    added routing for pages

--- boothbuddy/src/App.tsx
+++ boothbuddy/src/App.tsx
@@ -1,13 +1,22 @@
 import Home from "./pages/home.tsx"
+import Login from "./pages/login.tsx"
+import Download from "./pages/download.tsx"
+import Booth from "./pages/photobooth.tsx"
+import Signin from "./pages/signin.tsx"
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
-    <>
-      <div>
-        <Home />
-      </div>
-    </>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/booth" element={<Booth />} />
+        <Route path="/download" element={<Download />} />
+        <Route path="/signin" element={<Signin />} />
+      </Routes>
+    </HashRouter>
   )
 }
 
