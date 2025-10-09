18e82bebd95bc7e600c97351bda7d0955ff16a18
Olivia Laurel <OLaurel113@gmail.com>
2025-10-08 21:38:18 -0500

    buttons positions

--- boothbuddy/src/pages/home.tsx
+++ boothbuddy/src/pages/home.tsx
@@ -10,7 +10,7 @@ function Home() {
         <main className="flex-grow flex justify-center items-center">
           <div className="relative">
             <img src={Booth} alt="logo" className="fixed top-8 inset-x-0 mx-auto w-190 h-165" />
-            <div className="fixed inset-x-0 flex flex-col items-center justify-center space-y-4">
+            <div className="fixed top-107 left-80 inset-x-0 flex flex-row items-center justify-center space-x-4">
               <a href="/#/login" className="text-white bg-black px-4 py-2 rounded hover:bg-gray-800">
                 Login
               </a>
