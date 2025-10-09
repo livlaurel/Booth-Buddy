fe57e7e29c37111bfbd4200f3bf6d08562c67a01
Olivia Laurel <OLaurel113@gmail.com>
2025-10-08 21:28:09 -0500

    fixed img/footer

--- boothbuddy/src/components/header.tsx
+++ boothbuddy/src/components/header.tsx
@@ -2,10 +2,10 @@ import logo from "../imgs/boothbuddy_logo.png";
 
 const Header = () => {
     return (
-        <main className="bg-[#fff9f9] w-full fixed top-0 left-0">
+        <main className="w-full fixed top-0 left-0">
             <nav className="flex justify-between items-center ml-5">
                 <a href="/" className="text-black flex justify-left">
-                    <img src={logo} alt="logo" className="w-40 h-30" />
+                    <img src={logo} alt="logo" className="w-50 h-40" />
                 </a>
             </nav>    
         </main>
--- boothbuddy/src/pages/home.tsx
+++ boothbuddy/src/pages/home.tsx
@@ -8,12 +8,9 @@ function Home() {
       <div className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow flex justify-center items-center">
-          <img src={Booth} alt="logo" className="w-160 h-140" />
-          <div className="flex flex-col items-center space-y-4">
-            <h1 className="text-3xl font-black text-black">
-              Booth Buddy
-            </h1>
-            <div className="flex flex-col items-center space-y-2">
+          <div className="relative">
+            <img src={Booth} alt="logo" className="fixed top-8 inset-x-0 mx-auto w-190 h-165"/>
+            <div className="absolute inset-0 flex flex-col items-center justify-center space-y-4">
               <a href="/#/login" className="text-white bg-black px-4 py-2 rounded hover:bg-gray-800">
                 Login
               </a>
