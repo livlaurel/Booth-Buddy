fb1db34c75d4fbde0eb1211b37da8b2896d48ffc
Olivia Laurel <OLaurel113@gmail.com>
2025-10-08 21:30:57 -0500

    fixed buttons

--- boothbuddy/src/pages/home.tsx
+++ boothbuddy/src/pages/home.tsx
@@ -5,12 +5,12 @@ import Booth from "../imgs/homebooth.png"
 function Home() {
   return (
     <>
-      <div className="flex flex-col min-h-screen">
+      <div className="flex flex-col min-h-screen overflow-hidden">
         <Header />
         <main className="flex-grow flex justify-center items-center">
           <div className="relative">
-            <img src={Booth} alt="logo" className="fixed top-8 inset-x-0 mx-auto w-190 h-165"/>
-            <div className="absolute inset-0 flex flex-col items-center justify-center space-y-4">
+            <img src={Booth} alt="logo" className="fixed top-8 inset-x-0 mx-auto w-190 h-165" />
+            <div className="fixed inset-x-0 flex flex-col items-center justify-center space-y-4">
               <a href="/#/login" className="text-white bg-black px-4 py-2 rounded hover:bg-gray-800">
                 Login
               </a>
