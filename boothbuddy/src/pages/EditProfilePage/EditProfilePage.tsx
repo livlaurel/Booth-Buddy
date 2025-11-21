68b91a400dd953f1b5658860e48176e099170f01
Sebika-K <khulalsebika11@gmail.com>
2025-11-20 18:48:23 -0600

    add title and description section

--- boothbuddy/src/pages/EditProfilePage/EditProfilePage.tsx
+++ boothbuddy/src/pages/EditProfilePage/EditProfilePage.tsx
@@ -8,7 +8,11 @@ export default function EditProfilePage() {
 
       <main className="flex-1 flex justify-center px-4 sm:px-8 lg:px-16 py-10">
         <div className="w-full max-w-2xl">
-          {/* Content will go here */}
+            {/* TITLE + DESCRIPTION */}
+            <h1 className="text-3xl font-bold">Edit profile</h1>
+            <p className="text-gray-600 mt-1 mb-8">
+            Keep your personal details private.
+            </p>
         </div>
       </main>
 
