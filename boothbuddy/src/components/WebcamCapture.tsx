5deb7980e7e02b4a5eec039264c0b9e3d94d142c
Olivia Laurel <OLaurel113@gmail.com>
2025-12-07 14:58:16 -0600

    transparent countdown

--- boothbuddy/src/components/WebcamCapture.tsx
+++ boothbuddy/src/components/WebcamCapture.tsx
@@ -108,7 +108,7 @@ const WebcamCapture = forwardRef((props: WebcamCaptureProps, ref) => {
       />
       {countdown !== null && (
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 
-                        text-6xl font-bold text-white bg-black bg-opacity-50 rounded-full 
+                        text-6xl font-bold text-white bg-transparent rounded-full 
                         w-32 h-32 flex items-center justify-center">
           {countdown}
         </div>
